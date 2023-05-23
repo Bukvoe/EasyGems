ca8d75dae748a8ae33e6365bab2cfd98e661c561
bukvoe <bukvoe>
2023-05-23 09:44:23 +0300

    check active inventory tab is added

--- easy-gems.js
+++ easy-gems.js
@@ -1,1 +1,8 @@
-console.log('Easy Gems');
+function inventoryTabChanged() {
+  if (g_bViewingOwnProfile && g_ActiveInventory.m_appid === 753) {
+    console.log('Easy Gems');
+  }
+}
+
+window.addEventListener('hashchange', () => inventoryTabChanged());
+window.addEventListener('load', () => inventoryTabChanged());
