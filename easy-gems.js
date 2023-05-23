06bf0c3d1ca8bec1b2e4aeb7d3b7d7c9c5263e5a
bukvoe <bukvoe>
2023-05-23 09:56:24 +0300

    extension control panel is added

--- easy-gems.js
+++ easy-gems.js
@@ -1,6 +1,26 @@
+const panels = {
+  default: 'eg-default-panel',
+};
+
+const template = document.createElement('div');
+template.classList.add('eg-panel');
+template.innerHTML = 
+    `<div id="${panels.default}">
+        <button id="eg-selection-on" class="btn_large btn_green_white_innerfade"><span>Select items</span></button>
+    </div>`;
+
+const inventoryTab = document.getElementById('tabcontent_inventory');
+inventoryTab.insertBefore(template, inventoryTab.children[0]);
+
+function activatePanel(id) {
+  document.querySelectorAll('.eg-panel div').forEach((x) => { x.style.display = x.id === id ? 'flex' : 'none'; });
+}
+
 function inventoryTabChanged() {
   if (g_bViewingOwnProfile && g_ActiveInventory.m_appid === 753) {
-    console.log('Easy Gems');
+    activatePanel(panels.default);
+  } else {
+    activatePanel('');
   }
 }
 
