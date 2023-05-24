980c7a0748f053483b82f8c15dbf68406d09548f
bukvoe <bukvoe>
2023-05-24 11:20:30 +0300

    folders structure refactoring

--- src/scripts/injector.js
+++ src/scripts/injector.js
@@ -1,5 +1,5 @@
 const node = document.getElementsByTagName('head')[0];
 const script = document.createElement('script');
 script.setAttribute('type', 'text/javascript');
-script.setAttribute('src', chrome.runtime.getURL('easy-gems.js'));
+script.setAttribute('src', chrome.runtime.getURL('scripts/easy-gems.js'));
 node.appendChild(script);
